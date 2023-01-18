8540785161c8b16976e33263312f2b4e94be0fc9
Amanda Adams <amandabessadams@gmail.com>
2023-01-17 17:54:50 -0800

    Update platform

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "TokamakPublish",
-  platforms: [.macOS(.v11)],
+  platforms: [.macOS(.v12)],
   products: [
     .library(
       name: "TokamakPublish",
